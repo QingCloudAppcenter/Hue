ae1244ef4137f3b18f5239f670f4eba0c9ea9dcd
yanjun <yanjun@yunify.com>
2017-06-13 23:26:45 +0800

    accomplish the change in hue for hive

--- update_server.sh
+++ update_server.sh
@@ -4,14 +4,15 @@ if [ $# -lt 1 ];then
     echo error!
 else
     ssh $server "passwd ubuntu"
-    ssh $server "rm /usr/local/hadoop"
-    ssh $server "ln -s /usr/local/hadoop-2.7.2 /usr/local/hadoop"
     scp ../app-agent-linux-amd64.tar.gz $server:/usr/local/ 
     ssh $server "tar -zxf /usr/local/app-agent-linux-amd64.tar.gz -C /usr/local"
     ssh $server "rm /usr/local/app-agent-linux-amd64.tar.gz"
     ssh $server "cd /usr/local/app-agent-linux-amd64; ./install.sh"
     ssh $server "rm -r /usr/local/mysql/data"
-
+    
+    scp ../jar/mysql-connector-java-5.1.39-bin.jar $server:/usr/local/hive/lib/
+    scp ../jar/S3/* $server:/usr/local/hadoop/share/hadoop/common/lib/
+    scp code/profile $server:/etc/
     scp code/mysql.tgz $server:/usr/local/mysql/
     scp -r code/conf.d $server:/etc/confd/
     scp -r code/templates $server:/etc/confd/
